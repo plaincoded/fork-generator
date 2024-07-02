02f88246496c2d6a76701b7e39a96368bcbf2f2a
Kamil Jura <plaincoded@gmail.com>
2024-07-02 15:14:49 +0200

    untested

--- scripts/common.ts
+++ scripts/common.ts
@@ -14,26 +14,20 @@ export function getPoolsByAdapter(
   network: string
 ): { [key: string]: PoolByAdapter[] } {
   const files = fs.readdirSync("data");
-  const all = [];
   const byProtocol: { [key: string]: PoolByAdapter[] } = {};
 
   for (const file of files) {
     const content = fs.readFileSync(`data/${file}`, "utf-8");
     if (!JSON.parse(content)) continue;
 
-    const protocols = JSON.parse(content);
+    const contents = JSON.parse(content);
 
-    for (const protocol of protocols) {
-      if (protocol.adapterId === adapter && protocol.chain === network) {
-        all.push(protocol);
-      }
+    for (const pool of contents) {
+      // If it's not of our interest, continue loop and do nothing
+      if (pool.adapterId !== adapter && pool.chain !== network) continue;
+      if (!byProtocol[pool.protocolId]) byProtocol[pool.protocolId] = [];
+      byProtocol[pool.protocolId].push(pool as PoolByAdapter);
     }
-  }
-
-  for (const pool of all) {
-    if (!byProtocol[pool.protocolId]) byProtocol[pool.protocolId] = [];
-
-    byProtocol[pool.protocolId].push(pool as PoolByAdapter);
   }
 
   return byProtocol;
